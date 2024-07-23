5af8e1fc5a6bd6ca6575e0706f2ec12d7bb095de
Z-ajamy <abdorahman0283@gmail.com>
2024-07-23 18:08:56 +0300

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -37,12 +37,12 @@ class BaseModel:
         """Initialize a new BaseModel instance,
             setting its ID and timestamps."""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.utcnow()
-        self.updated_at = datetime.datetime.utcnow()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def save(self):
         """Update the `updated_at` timestamp to the current time."""
-        self.updated_at = datetime.datetime.utcnow()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """Convert the instance attributes to a dictionary format.
