612cfd0f0f84d7ace6ce9ef85c9a3ba380b2ecab
Z-ajamy <abdorahman0283@gmail.com>
2025-08-15 17:41:08 +0300

    /

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 import uuid
-import datetime
-
-
+from datetime import datetime
 class BaseModel:
-
     def __init__(self):
-        
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.utcnow()
-        self.updated_at = datetime.datetime.utcnow()
-
-    def save(self):
-        self.updated_at = datetime.datetime.utcnow()
-
-    def to_dict(self):
-        dect = self.__dict__.copy()
-        dect['created_at'] = self.created_at.isoformat()
-        dect['updated_at'] = self.updated_at.isoformat()
-        dect["__class__"] = type(self).__name__
-        return dect
-
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+        return("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+    def save(self):
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        dic = self.__dict__.copy()
+        dic["__class__"] = type(self).__name__
+        dic["created_at"] = datetime.isoformat(self.created_at)
+        dic["updated_at"] = datetime.isoformat(self.updated_at)
+        return dic
