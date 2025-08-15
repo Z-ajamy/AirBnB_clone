1816aba0fc80c252bcb2a249a11c65cca3c8444e
Z-ajamy <abdorahman0283@gmail.com>
2025-08-15 19:45:31 +0300

    ؟

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,45 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+
+
 class BaseModel:
+    """
+    Defines all common attributes/methods for other classes
+    """
+
     def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        """
+        Initializes the BaseModel instance
+        """
+        self.id = str(uuid.uuid4())  # Unique ID
+        self.created_at = datetime.now()  # Creation time
+        self.updated_at = datetime.now()  # Last update time
+
     def __str__(self):
-        return("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
+        """
+        String representation of the object
+        Format: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
-        self.updated_at = datetime.utcnow()
+        """
+        Updates the 'updated_at' attribute with the current datetime
+        """
+        self.updated_at = datetime.now()
+
     def to_dict(self):
-        dic = self.__dict__.copy()
-        dic["__class__"] = str(type(self).__name__)
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
-        return dic
-    
+        """
+        Returns a dictionary containing all keys/values of __dict__
+        - Adds __class__ key
+        - Converts datetime attributes to ISO format strings
+        """
+        # Copy all attributes
+        obj_dict = self.__dict__.copy()
+        # Add class name
+        obj_dict["__class__"] = self.__class__.__name__
+        # Convert datetime objects to string in ISO format
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
