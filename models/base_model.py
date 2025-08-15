e959fea8bc81ac40c322f09e60d597358d380c2b
Z-ajamy <abdorahman0283@gmail.com>
2025-08-15 19:43:34 +0300

    ?

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,3 @@
-'''#!/usr/bin/python3
-import uuid
-from datetime import datetime
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-    def __str__(self):
-        return("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
-    def save(self):
-        self.updated_at = datetime.utcnow()
-    def to_dict(self):
-        dic = self.__dict__.copy()
-        dic["__class__"] = str(type(self).__name__)
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
-        return dic
-    '''
-
-
-
 #!/usr/bin/python3
 """Defines the BaseModel class."""
 import models
