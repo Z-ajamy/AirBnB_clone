16dccd4b6d3d3f0434e6ae1dfb5488327dc34844
Z-ajamy <abdorahman0283@gmail.com>
2025-08-20 23:46:04 +0300

    FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-
-        for i in kwargs:
-            if i != "__class__":
-                if i == "created_at" or i == "updated_at":
-                    date = datetime.fromisoformat(kwargs[i])
-                    setattr(self, i, date)
-                else:
-                    setattr(self, i, kwargs[i])
+        if kwargs:
+            for i in kwargs:
+                if i != "__class__":
+                    if i == "created_at" or i == "updated_at":
+                        date = datetime.fromisoformat(kwargs[i])
+                        setattr(self, i, date)
+                    else:
+                        setattr(self, i, kwargs[i])
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     def save(self):
         self.updated_at = datetime.utcnow()
+        models.storage.save()
     def to_dict(self):
         dic = self.__dict__.copy()
         dic["__class__"] = self.__class__.__name__
---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import json
+from pathlib import Path
+from models.base_model import BaseModel
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = str(type(obj).__name__) + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        jsdata = {}
+        for i in FileStorage.__objects:
+            jsdata[i] = FileStorage.__objects[i].to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(jsdata, f)
+
+    def reload(self):
+        f_path = Path(self.__file_path)
+        if f_path.exists():
+            with open(self.__file_path, "r") as f:
+                jsdata = json.load(f)
+                for i in jsdata:
+                    temp = BaseModel(jsdata[i])
+                    self.__objects[i] = temp
