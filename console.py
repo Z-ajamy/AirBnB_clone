397ff38d9fa27f30990996e208532d4f1a26ef80
Z-ajamy <abdorahman0283@gmail.com>
2025-08-22 01:46:53 +0300

    create, show, destroy, all

--- console.py
+++ console.py
@@ -2,10 +2,17 @@
 """
 Entry point of the command interpreter for the HBNB project.
 """
+from models.base_model import BaseModel
+from models import storage
 
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+
+    set_of_classes = {
+        "BaseModel"
+    }
+
     prompt = "(hbnb) "
 
     def do_quit(self, line):
@@ -16,6 +23,94 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+#---------------------------------------------------------
+    def do_create(self, line):
+        args = line.split()
+        try:
+            cls_name = args[0]
+        except IndexError:
+            print("** class name missing **")
+            return
+        if cls_name == "BaseModel":
+            temp = BaseModel()
+            print(temp.id)
+            temp.save()
+        else:
+            print("** class doesn't exist **")
+
+#---------------------------------------------------------
+    def do_show(self, line):
+        args = line.split()
+        try:
+            cls_name = args[0]
+        except IndexError:
+            print("** class name missing **")
+            return
+        if cls_name not in self.set_of_classes:
+            print("** class doesn't exist **")
+            return
+        try:
+            id = args[1]
+        except IndexError:
+            print("** instance id missing **")
+            return
+        obj_id = "{}.{}".format(cls_name, id)
+        all_objects = storage.all()
+        if obj_id in all_objects:
+            print(all_objects[obj_id])
+        else:
+            print("** no instance found **")
+
+
+#---------------------------------------------------------
+    def do_destroy(self, line):
+        args = line.split()
+        try:
+            cls_name = args[0]
+        except IndexError:
+            print("** class name missing **")
+            return
+        if cls_name not in self.set_of_classes:
+            print("** class doesn't exist **")
+            return
+        try:
+            id = args[1]
+        except IndexError:
+            print("** instance id missing **")
+            return
+        obj_id = "{}.{}".format(cls_name, id)
+        all_objects = storage.all()
+        if obj_id in all_objects:
+            del all_objects[obj_id]
+        else:
+            print("** no instance found **")
+
+
+#---------------------------------------------------------
+    def do_all(self, line):
+        look_for = '.'
+        if line:
+            if line in self.set_of_classes:
+                look_for = line
+            else:
+                print("** class doesn't exist **")
+                return
+        all_objects = storage.all()
+        str_all = "["
+        spark = 1
+        for i in all_objects:
+            spark = 0
+            if look_for in i:
+                str_all += "\"" + str(all_objects[i].to_dict()) + "\", "
+        if spark:
+            str_all = "[]"
+        else:
+            str_all = str_all[:-2] + "]"
+        print(str_all)
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,7 @@ class BaseModel:
                     setattr(self, i, date)
                 else:
                     setattr(self, i, kwargs[i])
+        
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
