fb9056e78342aa1f68d897c4a4cadeaa366a3341
Z-ajamy <abdorahman0283@gmail.com>
2025-08-21 18:03:19 +0300

    GOOOOOOOOOOOOD

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,23 +15,20 @@ class FileStorage:
 
     def save(self):
         jsdata = {}
-        if FileStorage.__file_path:
-            for i in FileStorage.__objects:
-                jsdata[i] = FileStorage.__objects[i].to_dict()
-            with open(FileStorage.__file_path, "w") as f:
-                json.dump(jsdata, f)
+        for i in FileStorage.__objects:
+            jsdata[i] = FileStorage.__objects[i].to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(jsdata, f)
 
     def reload(self):
-        if FileStorage.__file_path:
-
-            f_path = Path(FileStorage.__file_path)
-            if f_path.exists():
-                with open(FileStorage.__file_path, "r") as f:
-                    jsdata = json.load(f)
-                    for i in jsdata:
-                        cls_name = jsdata[i]["__class__"]
-                        if cls_name == "BaseModel":
-                            from models.base_model import BaseModel
-                            obj = BaseModel(**jsdata[i])
-                            self.new(obj)
+        f_path = Path(FileStorage.__file_path)
+        if f_path.exists():
+            with open(FileStorage.__file_path, "r") as f:
+                jsdata = json.load(f)
+                for i in jsdata:
+                    cls_name = jsdata[i]["__class__"]
+                    if cls_name == "BaseModel":
+                        from models.base_model import BaseModel
+                        obj = BaseModel(**jsdata[i])
+                        self.new(obj)
 
