b8632a0cedc66d02637a30fd6a60ae0e802eaf8d
Z-ajamy <abdorahman0283@gmail.com>
2025-08-21 20:29:38 +0300

    emptyline -> pass

--- console.py
+++ console.py
@@ -6,17 +6,16 @@ Entry point of the command interpreter for the HBNB project.
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """Simple command interpreter for the HBNB project"""
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """Exit the program"""
         return True
 
     def do_EOF(self, line):
-        """Handle EOF (Ctrl+D) to exit"""
         return True
 
+    def emptyline(self):
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
