ff5a14329cabbfb50886ad61c08b9277140b1cc6
Z-ajamy <abdorahman0283@gmail.com>
2025-08-21 16:56:38 +0300

    ؟

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,34 +1,58 @@
 #!/usr/bin/python3
-import json
-from pathlib import Path
+"""
+BaseModel class for AirBnB project
+"""
+import uuid
+from datetime import datetime
+import models
 
-class FileStorage:
-    __file_path = "file.json"
-    __objects = {}
 
-    def all(self):
-        return FileStorage.__objects
+class BaseModel:
+    """Base class that defines common attributes/methods for other classes"""
 
-    def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization of the base model
+        - if kwargs is not empty → recreate instance from dict
+        - else → create new instance with id and timestamps
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+
+    def __str__(self):
+        """
+        Return string representation of the instance
+        Example: [BaseModel] (1234) {'id': '1234', ...}
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        jsdata = {}
-        for i in FileStorage.__objects:
-            jsdata[i] = FileStorage.__objects[i].to_dict()
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(jsdata, f)
+        """
+        Updates `updated_at` with the current datetime
+        and saves to storage
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
 
-    def reload(self):
-        f_path = Path(FileStorage.__file_path)
-        if f_path.exists():
-            with open(FileStorage.__file_path, "r") as f:
-                jsdata = json.load(f)
-                for i in jsdata:
-                    cls_name = jsdata[i]["__class__"]
-                    if cls_name == "BaseModel":
-                        from models.base_model import BaseModel
-                        obj = BaseModel(**jsdata[i])
-                        self.new(obj)
-
+    def to_dict(self):
+        """
+        Return a dictionary representation of the instance
+        - converts datetime to ISO string
+        - adds __class__ key
+        """
+        dict_repr = self.__dict__.copy()
+        dict_repr["__class__"] = self.__class__.__name__
+        dict_repr["created_at"] = self.created_at.isoformat()
+        dict_repr["updated_at"] = self.updated_at.isoformat()
+        return dict_repr
