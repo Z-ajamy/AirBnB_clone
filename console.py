396d51590f9b8a8413425de09c577ff4cc788744
Z-ajamy <abdorahman0283@gmail.com>
2025-08-21 20:20:02 +0300

    Cmd

--- console.py
+++ console.py
@@ -2,14 +2,17 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """Simple command interpreter for the HBNB project"""
     prompt = "(hbnb) "
+
     def do_quit(self, line):
+        """Exit the program"""
         return True
+
     def do_EOF(self, line):
+        """Handle EOF (Ctrl+D) to exit"""
         return True
-    def do_help(self, line):
-        print("\nDocumented commands (type help <topic>):" \
-        "\n========================================\n" \
-        "EOF  help  quit\n")
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
