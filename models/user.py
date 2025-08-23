454a7051ecd4da98d656c923ab68f7fdbe438a3c
Z-ajamy <abdorahman0283@gmail.com>
2025-08-23 22:53:11 +0300

    Interstellar

--- models/user.py
+++ models/user.py
@@ -3,20 +3,9 @@ from models.base_model import BaseModel
 from datetime import datetime
 
 class User(BaseModel):
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for i in kwargs:
-                if i == "__class__":
-                    continue
-                elif i == "created_at" or i == "updated_at":
-                    setattr(self, i, datetime.fromisoformat(kwargs[i]))
-                else:
-                    setattr(self, i, kwargs[i])
 
-        else:
-            self.email = ""
-            self.password = ""
-            self.first_name = ""
-            self.last_name = ""
-            super().__init__()
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
             
